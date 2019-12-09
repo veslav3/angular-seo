2712da5ebdbea126e470c8ae4f02745a69b24712
Roy Honders <roy.honders@luminis.eu>
2019-12-09 11:18:47 +0100

    Own implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
-import { NgtUniversalModule } from '@ng-toolkit/universal';
-import { CommonModule } from '@angular/common';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { MenuComponent } from './menu/menu.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {NgtUniversalModule} from '@ng-toolkit/universal';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
+import {MenuComponent} from './menu/menu.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
- declarations: [
-   AppComponent,
-   FirstComponent,
-   SecondComponent,
-   MenuComponent
- ],
- imports:[
- CommonModule,
-NgtUniversalModule,
- 
-   
-   AppRoutingModule,
-   HttpClientModule
- ],
- providers: [],
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    NgtUniversalModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {
+}
 
 
 
